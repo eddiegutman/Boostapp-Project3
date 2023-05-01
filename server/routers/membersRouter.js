77cae2f74a5e8cc86e403e9bb59999a46d57cfd4
Eddie Gutman <edigutman@gmail.com>
2023-05-02 00:57:29 +0300

    Add routers
    Added 3 routers:
    - membersRouter
    - moviesRouter
    - subscriptionsRouter

---
+++ server/routers/membersRouter.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const membersBLL = require('../BLL/membersBLL');
+
+const router = express.Router();
+
+// GET - get all members
+router.get("/", async (request, response) => {
+    try {
+        const members = await membersBLL.getAllMembers();
+        response.status(200).json(members);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// GET - get member by id
+router.get("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const member = await membersBLL.getMemberByID(id);
+        response.status(200).json(member);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// GET - get all members with their subscriptions
+router.get("/subs", async (request, response) => {
+    try {
+        const members = await membersBLL.getAllMembersWithSubs();
+        response.status(200).json(members);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// POST - add a new member
+router.post("/", async (request, response) => {
+    try {
+        const member = request.body;
+        const status = await membersBLL.addMember(member);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// PUT - update existing member
+router.put("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const member = request.body;
+        const status = await membersBLL.updateMember(id, member);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// DELETE - delete a customer
+router.delete("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const status = await membersBLL.deleteMember(id);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+module.exports = router;
---
+++ server/routers/moviesRouter.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const moviesBLL = require('../BLL/moviesBLL');
+
+const router = express.Router();
+
+// GET - get all movies
+router.get("/", async (request, response) => {
+    try {
+        const movies = await moviesBLL.getAllMovies();
+        response.status(200).json(movies);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// GET - get movie by id
+router.get("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const movie = await moviesBLL.getMovieByID(id);
+        response.status(200).json(movie);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// GET - get all movies with their subscribers
+router.get("/subs", async (request, response) => {
+    try {
+        const movies = await moviesBLL.getAllMoviesWithSubs();
+        response.status(200).json(movies);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// POST - add a new movie
+router.post("/", async (request, response) => {
+    try {
+        const movie = request.body;
+        const status = await moviesBLL.addMovie(movie);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// PUT - update existing movie
+router.put("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const movie = request.body;
+        const status = await moviesBLL.updateMovie(id, movie);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+// DELETE - delete a movie
+router.delete("/:id", async (request, response) => {
+    try {
+        const id = request.params.id;
+        const status = await moviesBLL.deleteMovie(id);
+        response.status(200).json(status);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+});
+
+module.exports = router;
