84a9ea6eba87bdeec7c575077ed9f3c0c02fe373
Eddie Gutman <edigutman@gmail.com>
2023-05-06 16:10:38 +0300

    Fix the types of subscriptionModel
    Fixed the types of movieID and memberID in subscriptionModel

--- server/models/subscriptionModel.js
+++ server/models/subscriptionModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const SubscriptionSchema = new mongoose.Schema({
-    movieID: { type: Schema.Types.ObjectId, ref: 'Movie' },
-    memberID: { type: Schema.Types.ObjectId, ref: 'Member' },
+    movieID: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
+    memberID: { type: mongoose.Schema.Types.ObjectId, ref: 'Member' },
     date: Date
 });
 const Subscription = mongoose.model('Subscription', SubscriptionSchema);
