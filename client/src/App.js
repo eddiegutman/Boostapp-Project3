83b850f4d65a9e23bda7272c8a69a3c8b27503fa
Eddie Gutman <edigutman@gmail.com>
2023-05-08 16:29:38 +0300

    Add basic main page for the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import Movies from './Components/Movies/Movies';
+import AddMovie from './Components/Movies/AddMovie';
+import MovieList from './Components/Movies/MovieList';
+import Subscriptions from './Components/Subscriptions/Subscriptions';
+
+import { getAllMoviesWithSubs } from './libs/movieUtils';
+import { getAllMembersWithSubs } from './libs/membersUtils';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    const movies = await getAllMoviesWithSubs();
+    const members = await getAllMembersWithSubs();
+    const data = {
+      movies: movies,
+      members: members
+    }
+    dispatch({ type: 'LOAD', payload: data });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movies - Subscriptions Web Site</h1>
+      <Link to='/movies'>Movies</Link> &nbsp;
+      <Link to='/subscriptions'>Subscriptions</Link> <br />
+      <div style={{ border: '2px solid black' }}>
+        <Routes>
+          <Route path='/movies' element={<Movies />} >
+            <Route path='all' element={<MovieList />} />
+            <Route path='addMovie' element={<AddMovie />} />
+          </Route>
+          <Route path='/subscriptions' element={<Subscriptions />} />
+        </Routes>
+      </div>
     </div>
   );
 }
