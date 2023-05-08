637f8a9096839d454d98d526554a79cbca41bb70
Eddie Gutman <edigutman@gmail.com>
2023-05-08 21:39:50 +0300

    Update appReducer to include Member function

--- client/src/Redux/appReducer.js
+++ client/src/Redux/appReducer.js
@@ -22,7 +22,25 @@ function appReducer(state = { movies: [], members: [] }, action) {
             const id = action.payload;
             const copy = state.movies.filter(entry => entry.movie._id !== id);
             return { movies: [...copy], members: [...state.members] };
-
+        }
+        case 'ADD_MEMBER': {
+            const member = action.payload;
+            const entry = {
+                member: member,
+                subscriptions: []
+            }
+            return { movies: [...state.movies], members: [...state.members, entry] };
+        }
+        case 'UPDATE_MEMBER': {
+            const member = action.payload;
+            const index = state.members.findIndex(entry => entry.member._id === member._id);
+            state.members[index].member = { ...member };
+            return { movies: [...state.movies], members: [...state.members] };
+        }
+        case 'DELETE_MEMBER': {
+            const id = action.payload;
+            const copy = state.members.filter(entry => entry.member._id !== id);
+            return { movies: [...state.movies], members: [...copy] };
         }
         default:
             return state;
