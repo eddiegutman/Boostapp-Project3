bf39aacd130b8fa1bc690c3430aafba6ea8ba9c7
Eddie Gutman <edigutman@gmail.com>
2023-05-14 02:40:53 +0300

    Add authetication router

---
+++ server/routers/auththenticationRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+// POST - login a user
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login/failed' }), async (request, response) => {
+    response.status(200).json({
+        success: true,
+        message: 'Login successful',
+        userData: {
+            id: request.user._id,
+            fullname: request.user.fullname,
+            email: request.user.email
+        }
+    });
+});
+
+// GET - login failed 
+router.get("/login/failed", async (request, response) => {
+    response.json({
+        success: false,
+        message: 'Username or password is incorrect'
+    });
+});
+
+// GET - login failed 
+router.get('/logout', (request, response, next) => {
+    request.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        response.json({
+            success: true,
+            message: 'Logged out'
+        });
+    });
+});
+
+// checks user authentication
+router.get("/checkAuthentication", (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.json({ authenticate: true, message: 'welcome' });
+    } else {
+        return res.json({ authenticate: false, message: 'No permission' });
+    }
+});
+
+module.exports = router;
