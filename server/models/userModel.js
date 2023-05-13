f768087c706795fd493123ff654b3148115473d0
Eddie Gutman <edigutman@gmail.com>
2023-05-14 02:22:15 +0300

    Update user model to contain email

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-    fullname: String
+    fullname: String,
+    email: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
