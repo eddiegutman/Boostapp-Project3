a592f9421aeee468ac331ce1bfff9fffa292528b
Eddie Gutman <edigutman@gmail.com>
2023-05-14 17:13:54 +0300

    Add login registration and navigation components

---
+++ client/src/Components/Main/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../../libs/authUtils';
+import { AuthContext } from '../../App';
+
+function NavBar() {
+    const { authentication, setAuthentication } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        const status = await logout();
+        if (status.success) {
+            setAuthentication({ success: false, user: null })
+            navigate('/login');
+        }
+    }
+
+    return (
+        <div>
+            <Link to='/movies/all'>Movies</Link> &nbsp;
+            <Link to='/members/all'>Members</Link> &nbsp;
+            {authentication.success ? <span>
+                <span>Hello, {authentication.user.fullname}</span> &nbsp;
+                <button onClick={handleLogout}>Logout</button>
+            </span>
+                : <span>
+                    <Link to='/login'>Login</Link> &nbsp;
+                    <Link to='/register'>Register</Link>
+                </span>}
+        </div>
+    )
+}
+
+export default NavBar;
