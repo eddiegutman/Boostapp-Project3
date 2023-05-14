ef978c554874e27f6ec8095c5aa51f2c454bc03c
Eddie Gutman <edigutman@gmail.com>
2023-05-14 19:43:10 +0300

    Update registration form to confirm password

--- client/src/Components/Main/Register.js
+++ client/src/Components/Main/Register.js
@@ -7,20 +7,25 @@ import { AuthContext } from '../../App';
 
 function Register() {
     const [user, setUser] = useState({ username: '', fullname: '', email: '', password: '' });
+    const [confirm, setConfirm] = useState('');
     const { setAuthentication } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const status = await register(user);
-        alert(status.message);
-        if (status.success) {
-            const authStatus = await authenticate();
-            if (authStatus.authenticate) {
-                setAuthentication({ success: true, user: authStatus.userData })
-                navigate('/movies/all');
+        if (user.password === confirm) {
+            const regStatus = await register(user);
+            alert(regStatus.message);
+            if (regStatus.success) {
+                const authStatus = await authenticate();
+                if (authStatus.authenticate) {
+                    setAuthentication({ success: true, user: authStatus.userData })
+                    navigate('/movies/all');
+                }
             }
+        } else {
+            alert('Passwords do not match!');
         }
     }
 
@@ -36,6 +41,8 @@ function Register() {
                 <input type={'email'} id='email' onChange={e => setUser({ ...user, email: e.target.value })} required /> <br />
                 <label htmlFor='password'>Password : </label>
                 <input type={'password'} id='password' onChange={e => setUser({ ...user, password: e.target.value })} required minLength={6} maxLength={12} /> <br />
+                <label htmlFor='confirm'>Confirm Password : </label>
+                <input type={'password'} id='confirm' onChange={e => setConfirm(e.target.value)} required minLength={6} maxLength={12} /> <br />
                 <input type={'submit'} value={'Sign up'} /> <br />
                 <span>Already have an account? <Link to='/login' >Sign in</Link></span>
             </form>
