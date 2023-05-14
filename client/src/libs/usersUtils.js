8897c71353cf193b73d55981dbcbd1066e12ef84
Eddie Gutman <edigutman@gmail.com>
2023-05-14 19:42:35 +0300

    Update users utils
    Added the following functions:
    - updateUserData
    - changePassword
    - deleteAccount

--- client/src/libs/usersUtils.js
+++ client/src/libs/usersUtils.js
@@ -2,12 +2,33 @@ import axios from 'axios';
 
 const url = 'http://localhost:8000/users'
 
-// GET - get all members
+// register
 const register = async (user) => {
     const { data } = await axios.post(`${url}/register`, user, { withCredentials: true });
     return data;
 }
 
+// update user's data
+const updateUserData = async (id, userData) => {
+    const { data } = await axios.put(`${url}/${id}`, userData, { withCredentials: true });
+    return data;
+}
+
+// change user's password
+const changePassword = async (id, user) => {
+    const { data } = await axios.post(`${url}/changePassword/${id}`, user, { withCredentials: true });
+    return data;
+}
+
+// delete account
+const deleteAccount = async (id) => {
+    const { data } = await axios.delete(`${url}/${id}`, { withCredentials: true });
+    return data;
+}
+
 export {
-    register
+    register,
+    updateUserData,
+    changePassword,
+    deleteAccount
 }
